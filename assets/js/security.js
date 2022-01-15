bca0d16c9870fa5ef6450022bdd910d3f37c137c
LBRfnc <91198455+LBRfnc@users.noreply.github.com>
2022-01-15 23:09:28 +0100

    Update security.js

--- assets/js/security.js
+++ assets/js/security.js
@@ -1,3 +1,8 @@
+/**
+* Template Name: Francesco Libraro
+* Template URL: https://climalibraro.it
+* Author: Francesco Libraro
+*/
 if (top.location != self.location) {
     top.location = self.location.href;
 }
